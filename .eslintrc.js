9d68eaff6bfa4bb65b1e01bab693ba7b21ca03ca
ijk <ijkten@no-email.com>
2020-03-14 15:12:22 -0400

    NodeJS with hapi project template

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  extends: ['eslint:recommended', 'google'],
+  env: {
+    jest: true,
+    node: true,
+    es6: true
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
+    'comma-dangle': ['error', 'never'],
+    'prefer-template': 'error',
+    'complexity': ['error', 50],
+    'max-len': ['error', {code: 120}]
+  }
+}
+  
---
+++ README.md
@@ -0,0 +1,11 @@
+# Get the modules
+```npm install```
+
+# Run the project
+```node index.js```
+
+# Run in production
+```RUN npm ci --only=production```
+
+## Access the API via a browser
+Go to http://localhost:8080/documentation
